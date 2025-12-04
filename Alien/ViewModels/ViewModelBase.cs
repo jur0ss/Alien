3786998d2935b631efe6e076d4f2a61c62e3dd8b
jurekg <jurekgz27@gmail.com>
2025-12-04 10:22:39 +0100

    First

---
+++ Alien/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Alien.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Alien/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Alien.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
